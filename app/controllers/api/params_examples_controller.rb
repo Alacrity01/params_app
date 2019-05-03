71ac3102fb3023039442671a47f588ddec64085d
TheAlacrity <brinker.jeff@gmail.com>
2019-05-03 17:23:43 -0500

    initial commit

--- app/controllers/api/params_examples_controller.rb
+++ app/controllers/api/params_examples_controller.rb
@@ -5,4 +5,15 @@ class Api::ParamsExamplesController < ApplicationController
     @message_3 = params["message_3"]
     render 'query_params.json.jbuilder'
   end
+
+  def segment_params
+    @message = params[:wildcard]
+    render 'segment_params.json.jbuilder'
+  end
+
+  def body_params
+    input_value = params[:secret_info]
+    @output_message = "The secret message is #{input_value}"
+    render 'body_params.json.jbuilder'
+  end
 end
